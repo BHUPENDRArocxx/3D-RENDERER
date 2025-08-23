5aa7ffc390aba8a5ae8901e3433793106196e829
BHUPENDRArocxx <bhupendrasinghrocxxx@gmail.com>
2025-08-23 12:49:55 +0530

    initial commit

---
+++ DemoViewer.java
@@ -0,0 +1,86 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.Path2D;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DemoViewer {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        Container pane = frame.getContentPane();
+        pane.setLayout(new BorderLayout());
+
+        // Slider to control horizontal rotation
+        JSlider headingSlider = new JSlider(0, 360, 180);
+        pane.add(headingSlider, BorderLayout.SOUTH);
+
+        // Slider to control vertical rotation
+        JSlider pitchSlider = new JSlider(SwingConstants.VERTICAL, -90, 90, 0);
+        pane.add(pitchSlider, BorderLayout.EAST);
+
+        // Panel to display render results
+        JPanel renderPanel = new JPanel() {
+            public void paintComponent(Graphics g) {
+                Graphics2D g2 = (Graphics2D) g;
+                g2.setColor(Color.BLACK);
+                g2.fillRect(0, 0, getWidth(), getHeight());
+
+                // Define 3D objects (a tetrahedron in this case)
+                List<Triangle> tris = new ArrayList<>();
+                tris.add(new Triangle(new Vertex(100, 100, 100),
+                                      new Vertex(-100, -100, 100),
+                                      new Vertex(-100, 100, -100),
+                                      Color.WHITE));
+                tris.add(new Triangle(new Vertex(100, 100, 100),
+                                      new Vertex(-100, -100, 100),
+                                      new Vertex(100, -100, -100),
+                                      Color.RED));
+                tris.add(new Triangle(new Vertex(-100, 100, -100),
+                                      new Vertex(100, -100, -100),
+                                      new Vertex(100, 100, 100),
+                                      Color.GREEN));
+                tris.add(new Triangle(new Vertex(-100, 100, -100),
+                                      new Vertex(100, -100, -100),
+                                      new Vertex(-100, -100, 100),
+                                      Color.BLUE));
+
+                g2.translate(getWidth() / 2, getHeight() / 2);
+                
+                // Draw each triangle
+                for (Triangle t : tris) {
+                    g2.setColor(t.color);
+                    Path2D path = new Path2D.Double();
+                    path.moveTo(t.v1.x, t.v1.y);
+                    path.lineTo(t.v2.x, t.v2.y);
+                    path.lineTo(t.v3.x, t.v3.y);
+                    path.closePath();
+                    g2.draw(path);
+                }
+            }
+        };
+        pane.add(renderPanel, BorderLayout.CENTER);
+
+        frame.setSize(400, 400);
+        frame.setVisible(true);
+    }
+}
+
+class Vertex {
+    double x, y, z;
+    Vertex(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
+
+class Triangle {
+    Vertex v1, v2, v3;
+    Color color;
+    Triangle(Vertex v1, Vertex v2, Vertex v3, Color color) {
+        this.v1 = v1;
+        this.v2 = v2;
+        this.v3 = v3;
+        this.color = color;
+    }
+}
